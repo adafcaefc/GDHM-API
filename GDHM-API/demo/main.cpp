e7b3d072918f10e4cabee4a93541565c378f5e3d
Juan Vincent Nugrahaputra <kjuanvincent@gmail.com>
2021-10-14 09:07:43 +0700

    patch for GDHM v32.62

--- GDHM-API/demo/main.cpp
+++ GDHM-API/demo/main.cpp
@@ -3,15 +3,17 @@
 #include <gdhm.h>
 
 static unsigned int counter = 0;
-static bool _init = false;
+
 void _main_callback()
 {
 	// Example of initialising
+	static bool _init = false;
 	if (!_init)
 	{
-		MessageBoxA(NULL, "Welcome :)", "GDHM API Initialised", NULL);
+		MessageBoxA(NULL, "Welcome :)", "gdhm API Initialised", NULL);
 		_init = true;
 	}
+
 	// Example of callback function usage
 	++counter;
 	if (counter > 360u) counter = 0;
@@ -20,83 +22,88 @@ void _main_callback()
 void _footer()
 {
 	// Example of footer label
-	GDHM::add_label("Test Footer Label");
-	GDHM::add_label("");
+	gdhm::add_label("Test Footer Label");
+	gdhm::add_label("");
 	
 	// Example of getting windows size
-	if (GDHM::add_button("Check Size"))
+	if (gdhm::add_button("Check Size"))
 	{
-		const auto _size = GDHM::get_window_size();
+		const auto _size = gdhm::get_window_size();
 		MessageBoxA(NULL, std::to_string(_size.x).c_str(), "x size", NULL);
 		MessageBoxA(NULL, std::to_string(_size.y).c_str(), "y size", NULL);
 	}
 
 	// Example of getting windows position
-	if (GDHM::add_button("Check Position"))
+	if (gdhm::add_button("Check Position"))
 	{
-		const auto _size = GDHM::get_window_position();
+		const auto _size = gdhm::get_window_position();
 		MessageBoxA(NULL, std::to_string(_size.x).c_str(), "x position", NULL);
 		MessageBoxA(NULL, std::to_string(_size.y).c_str(), "y position", NULL);
 	}
 
 	// Example of setting windows position
-	if (GDHM::add_button("Set Position"))
+	if (gdhm::add_button("Set Position"))
 	{
-		GDHM::set_window_position(0.f, 0.f);
+		gdhm::set_window_position(0.f, 0.f);
 	}
-
-	// Example of setting windows position
-	if (GDHM::add_button("Set Size"))
-	{
-		//GDHM::set_window_size(100.f, 500.f);
-		GDHM::set_window_size_y(200.f);
-	}
-	GDHM::add_label("");
+	gdhm::add_label("");
 
 	// Example of showing counter using label
-	GDHM::add_label("Counter");
-	GDHM::add_label(std::to_string(counter).c_str());
-	GDHM::add_label("");
+	gdhm::add_label("Counter");
+	gdhm::add_label(std::to_string(counter).c_str());
+	gdhm::add_label("");
 }
 
 void _header()
 {
 	// Example of label
-	GDHM::add_label("Test Label");
-	GDHM::add_label("");
+	gdhm::add_label("Test Label");
+	gdhm::add_label("GDHM version:");
+	gdhm::add_label(gdhm::get_version().c_str());
+	gdhm::add_label("");
 
 	// Example of button
-	if (GDHM::add_button("Test Button"))
+	if (gdhm::add_button("Test Button"))
 	{
 		MessageBoxA(NULL, "Hello!", "A button", NULL);
 	}
+
 	// Example of checkbox
 	static bool _checkbox = false;
-	if (GDHM::add_checkbox("Test Checkbox", &_checkbox))
+	if (gdhm::add_checkbox("Test Checkbox", &_checkbox))
 	{
 		MessageBoxA(NULL, _checkbox ? "True" : "False", "The value is", NULL);
 	}
-	GDHM::add_label("");
+	gdhm::add_label("");
 
 	// Example of text box
 	static char _text_box[10];
-	GDHM::add_text_box("", _text_box, 10, "TEST_TEXT_BOX");
-	if (GDHM::last_item_just_released())
+	gdhm::add_text_box("", _text_box, 10, "TEST_TEXT_BOX");
+	if (gdhm::last_item_just_released())
 	{
 		MessageBoxA(NULL, _text_box, "The value is", NULL);
 	}
-	GDHM::add_label("");
+	gdhm::add_label("");
+
+	// Example of drag float
+	static float _drag_test = 1.f;
+	gdhm::drag_float(&_drag_test);
+	if (gdhm::last_item_just_released())
+	{
+		MessageBoxA(NULL, std::to_string(_drag_test).c_str(), "The value is", NULL);
+	}
+	gdhm::add_label("");
 }
 
 DWORD WINAPI _dll_main(LPVOID lpParam)
 {
 	// Example of loader
-	if (GDHM::is_gdhm_loaded())
+	if (gdhm::is_gdhm_loaded())
 	{
 		// Example of normal window
-		GDHM::add_mod_window("GDHM API Demo", 3.f, "", _main_callback, _header, _footer, 0.f, 400.f);
+		gdhm::add_mod_window("GDHM API DEMO", "", nullptr, _main_callback, _header, _footer);
 		// Example of mod window
-		GDHM::add_mod_window("GDHM API Mod Demo", 4.f, "dummy.json");
+		gdhm::add_mod_window("GDHM API MOD DEMO", "dummy.json", nullptr);
 	}
 	else
 	{
--- includes/gdhm.h
+++ includes/gdhm.h
@@ -1,28 +1,39 @@
 #ifndef GDHM_API_H
 #define GDHM_API_H
 
-#pragma comment (lib, "ToastedMarshmellow.lib")
-#define GDHM_API __declspec(dllimport)
+#if GDHM_API_IMPL == 1
+	#define GDHM_API __declspec(dllexport)
+#else
+	#pragma comment (lib, "ToastedMarshmellow.lib")
+	#define GDHM_API __declspec(dllimport)
+#endif
 
-namespace GDHM
+namespace gdhm
 {
-	struct TMVec2
+	struct Vec2
 	{
-		float x, y;
-		TMVec2() { x = y = 0.f; }
-		TMVec2(const float _x, const float _y) : x(_x), y(_y) {}
+		float
+			x = 0.f,
+			y = 0.f;
+		Vec2() { x = y = 0.f; }
+		Vec2(const float _x, const float _y) : x(_x), y(_y) {}
+		#ifdef GDHM_API_USE_IMGUI
+		Vec2(const ImVec2& v) : x(v.x), y(v.y) {}
+		#endif
 	};
 
+	#if GDHM_API_IMPL != 1
 	// This will add a new window in GDHM
 	GDHM_API void add_mod_window(
 		const char* name,
-		const float _index = 0.f,
-		const char* mod_file_name = "",
+		const char* mod_file_name,
+		const char* tag = nullptr,
 		void* callback = nullptr,
 		void* header = nullptr,
 		void* footer = nullptr,
-		const float extra_width = 0.f,
-		const float extra_height = 0.f);
+		bool* handle = nullptr,
+		const bool constraint_height = false,
+		const float max_height = 0.f);
 
 	// These will add common controls
 	GDHM_API bool add_checkbox(const char* label, bool* handle);
@@ -33,27 +44,47 @@ namespace GDHM
 		char* buffer,
 		const size_t buffer_size,
 		const char* tag = nullptr);
+	GDHM_API bool add_text_box(
+		char* buffer,
+		const size_t buffer_size,
+		const char* tag = nullptr);
+	// drag_float will be changed to add_drag_float on version above v32.62
+	GDHM_API void drag_float(
+		float* value,
+		const float v_speed = 1.0f,
+		const float v_min = 0.0f,
+		const float v_max = 0.0f,
+		const char* label_format = "%.3f",
+		const char* tag = nullptr,
+		void* on_release = nullptr);
+	GDHM_API void drag_float(
+		const char* label,
+		float* value,
+		const float v_speed = 1.0f,
+		const float v_min = 0.0f,
+		const float v_max = 0.0f,
+		const char* label_format = "%.3f",
+		const char* tag = nullptr,
+		void* on_release = nullptr);
 
 	// This will check whether the last item has just released
 	GDHM_API bool last_item_just_released();
 
-	// These will set window position
+	// These will manage window position
 	GDHM_API void set_window_position_x(const float x_value);
 	GDHM_API void set_window_position_y(const float y_value);
 	GDHM_API void set_window_position(const float x_value, const float y_value);
-	GDHM_API TMVec2 get_window_position();
+	GDHM_API Vec2 get_window_position();
 
-	// These will set window size
-	GDHM_API void set_window_size_x(const float x_value);
-	GDHM_API void set_window_size_y(const float y_value);
-	GDHM_API void set_window_size(const float x_value, const float y_value);
-	GDHM_API TMVec2 get_window_size();
+	// These will manage window size
+	GDHM_API Vec2 get_window_size();
 
 	// This will check whether GDHM is loaded
-	static bool is_gdhm_loaded()
-	{
-		return GetModuleHandle(TEXT("ToastedMarshmellow.dll"));
-	}
+	static inline bool is_gdhm_loaded() { return GetModuleHandle(TEXT("ToastedMarshmellow.dll")); }
+
+	// This will check the current GDHM version
+	GDHM_API std::string get_version();
+	#endif
 }
 
 #undef GDHM_API
