d1da79ce95852f6b3d5acc7b82c1bbba0039e4e3
Juan Vincent Nugrahaputra <kjuanvincent@gmail.com>
2021-11-15 21:26:19 +0700

    patch for GDHM v32.72, soft launch

--- GDHM-API/demo/main.cpp
+++ GDHM-API/demo/main.cpp
@@ -2,108 +2,142 @@
 #include <string>
 #include <gdhm.h>
 
-static unsigned int counter = 0;
-
+static unsigned int my_counter = 0;
 void _main_callback()
 {
-	// Example of initialising
-	static bool _init = false;
-	if (!_init)
-	{
-		MessageBoxA(NULL, "Welcome :)", "gdhm API Initialised", NULL);
-		_init = true;
-	}
-
 	// Example of callback function usage
-	++counter;
-	if (counter > 360u) counter = 0;
+	++my_counter;
+	if (my_counter > 360u) my_counter = 0u;
+}
+
+static void callback_1()
+{
+	// Functions here will be triggered both on click and on hotkey press
+	MessageBoxA(NULL, "callback_1", "hello", NULL);
+}
+
+static void callback_2()
+{
+	// Functions here will be triggered both on click and on hotkey press
+	MessageBoxA(NULL, "callback_2", "hello", NULL);
+}
+
+static void callback_3()
+{
+	// Functions here will be triggered both on click and on hotkey press
+	MessageBoxA(NULL, "callback_3", "hello", NULL);
 }
 
 void _footer()
 {
 	// Example of footer label
-	gdhm::add_label("Test Footer Label");
-	gdhm::add_label("");
+	gdhm::gui::label("Test Footer Label");
+	gdhm::gui::label("");
 	
-	// Example of getting windows size
-	if (gdhm::add_button("Check Size"))
-	{
-		const auto _size = gdhm::get_window_size();
-		MessageBoxA(NULL, std::to_string(_size.x).c_str(), "x size", NULL);
-		MessageBoxA(NULL, std::to_string(_size.y).c_str(), "y size", NULL);
-	}
+	// Example of adding buttons using variables
+	static std::uint8_t button_1_key = 0;
+	gdhm::gui::button(
+		gdhm_new_id(),
+		"Button 1",
+		&button_1_key,
+		callback_1,
+		"Number one info.");
 
-	// Example of getting windows position
-	if (gdhm::add_button("Check Position"))
-	{
-		const auto _size = gdhm::get_window_position();
-		MessageBoxA(NULL, std::to_string(_size.x).c_str(), "x position", NULL);
-		MessageBoxA(NULL, std::to_string(_size.y).c_str(), "y position", NULL);
-	}
+	// Example of adding buttons using struct with info
+	static gdhm::control button_2("Button 2", "Number two info.");
+	gdhm::gui::button(
+		gdhm_new_id(),
+		&button_2,
+		callback_2);
 
-	// Example of setting windows position
-	if (gdhm::add_button("Set Position"))
-	{
-		gdhm::set_window_position(0.f, 0.f);
-	}
-	gdhm::add_label("");
+	// Example of adding buttons using struct without info
+	static gdhm::control button_3("Button 3");
+	gdhm::gui::button(
+		gdhm_new_id(),
+		&button_3,
+		callback_3);
+	gdhm::gui::label("");
 
 	// Example of showing counter using label
-	gdhm::add_label("Counter");
-	gdhm::add_label(std::to_string(counter).c_str());
-	gdhm::add_label("");
+	gdhm::gui::label("Counter");
+	gdhm::gui::label(std::to_string(my_counter).c_str());
 }
 
 void _header()
 {
 	// Example of label
-	gdhm::add_label("Test Label");
-	gdhm::add_label("GDHM version:");
-	gdhm::add_label(gdhm::get_version().c_str());
-	gdhm::add_label("");
+	gdhm::gui::label("Test Label");
+	gdhm::gui::label("GDHM version:");
+	gdhm::gui::label(gdhm::get_version().c_str());
+	gdhm::gui::label("");
 
+	static gdhm::control button_1("Test Button");
 	// Example of button
-	if (gdhm::add_button("Test Button"))
-	{
-		MessageBoxA(NULL, "Hello!", "A button", NULL);
-	}
+	gdhm::gui::button(
+		gdhm_new_id(),
+		"Test Button",
+		nullptr, // no hotkey option is applied by passing nullptr to the hotkey option
+		[]() { MessageBoxA(NULL, "Hello!", "A button", NULL); });
 
 	// Example of checkbox
-	static bool _checkbox = false;
-	if (gdhm::add_checkbox("Test Checkbox", &_checkbox))
-	{
-		MessageBoxA(NULL, _checkbox ? "True" : "False", "The value is", NULL);
-	}
-	gdhm::add_label("");
+	static bool _checkbox_value = false;
+	gdhm::gui::checkbox(
+		gdhm_new_id(), 
+		"Test Checkbox", 
+		&_checkbox_value, 
+		nullptr, // no hotkey option is applied by passing nullptr to the hotkey option
+		[]() { MessageBoxA(NULL, _checkbox_value ? "True" : "False", "The value is", NULL); });
+
+	// Example of checkbox without callback
+	static bool _checkbox_value_2 = false;
+	gdhm::gui::checkbox(
+		gdhm_new_id(),
+		"Test Checkbox 2",
+		&_checkbox_value_2);
+
+	gdhm::gui::label("");
 
 	// Example of text box
 	static char _text_box[10];
-	gdhm::add_text_box("", _text_box, 10, "TEST_TEXT_BOX");
-	if (gdhm::last_item_just_released())
-	{
-		MessageBoxA(NULL, _text_box, "The value is", NULL);
-	}
-	gdhm::add_label("");
+	gdhm::gui::input_text(
+		gdhm_new_id(),
+		_text_box, 
+		sizeof(_text_box),
+		[]() { MessageBoxA(NULL, _text_box, "The value is", NULL); });
+	gdhm::gui::label("");
 
 	// Example of drag float
 	static float _drag_test = 1.f;
-	gdhm::add_drag_float(&_drag_test);
-	if (gdhm::last_item_just_released())
-	{
-		MessageBoxA(NULL, std::to_string(_drag_test).c_str(), "The value is", NULL);
-	}
-	gdhm::add_label("");
+	gdhm::gui::input_float(
+		gdhm_new_id(), 
+		&_drag_test,
+		"Float Input %.2f",
+		1.f,
+		0.f,
+		0.f,
+		[&]() { MessageBoxA(NULL, std::to_string(_drag_test).c_str(), "The value is", NULL); });
+	gdhm::gui::label("");
+
+	// Example of list box
+	static std::vector<std::string> items = { "Item 1", "Item 2", "Item 3" };
+	static std::uint8_t selected = NULL;
+	gdhm::gui::list_box(
+		gdhm_new_id(), 
+		&selected, items, 
+		[&]() { MessageBoxA(NULL, items.at(selected).c_str(), "You selected", NULL); });
+	gdhm::gui::label("");
 }
 
 DWORD WINAPI _dll_main(LPVOID lpParam)
 {
 	// Example of loader
-	if (gdhm::is_gdhm_loaded())
+	if (gdhm::is_loaded())
 	{
 		// Example of normal window
-		gdhm::add_mod_window("GDHM API DEMO", "", nullptr, _main_callback, _header, _footer);
+		gdhm::gui::window("GDHM API DEMO", "", nullptr, _main_callback, _header, _footer);
+
 		// Example of mod window
-		gdhm::add_mod_window("GDHM API MOD DEMO", "dummy.json", nullptr);
+		gdhm::gui::window("GDHM API MOD DEMO", "dummy.json", nullptr);
 	}
 	else
 	{
